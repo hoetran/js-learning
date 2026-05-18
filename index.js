c1475db84bb785f86d350f933ce06ac6fc627df1
hoetran <tranvanhoe304@gmail.com>
2026-05-18 15:41:22 +0700

    Lesson 38: Objects

---
+++ index.js
@@ -0,0 +1,16 @@
+// object = A collection of related properties and/or methods
+//                Can represent real world objects (people, products, places)
+//                object = {key:value,
+//                               function()}
+
+const person1 = {
+    firstName: "Spongebob",
+    lastName: "Squarepants",
+    age: 30,
+    isEmployed: true,
+    sayHello: function(){console.log('Hi! I am Spongebob!')},
+    eat: function(){console.log("I am eating a Krabby Patty")},
+}
+
+console.log(person1.firstName);
+person1.sayHello();
