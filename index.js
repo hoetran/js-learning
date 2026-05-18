37f9a5af7562b0103c4c72e902e3dff23c8738ed
hoetran <tranvanhoe304@gmail.com>
2026-05-18 15:34:22 +0700

    Lesson 37: Arrow function

---
+++ index.js
@@ -0,0 +1,18 @@
+const hello = (name, age) => {console.log(`Hello ${name}`)
+console.log(`You are ${age} years old`)};
+
+hello("Harry", 25);
+
+setTimeout(() => {console.log("Hello");
+    console.log("Goodby")
+}, 3000);
+
+const numbers = [1,2,3,4,5,6];
+
+const squares = numbers.map((element) => Math.pow(element, 2));
+const cubes = numbers.map((element) => Math.pow(element, 3));
+const evenNums = numbers.filter((element) => element % 2 === 0);
+console.log(evenNums);
+const oddNums = numbers.filter((element) => element % 2 !== 0);
+const total = numbers.reduce((accumulator, element) => accumulator + element);
+console.log(total);
