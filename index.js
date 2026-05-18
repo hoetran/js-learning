2b938fef23d37f4513e9ceea7ae244daaefdce3e
hoetran <tranvanhoe304@gmail.com>
2026-05-18 15:37:54 +0700

    Lesson 37: Arrow function

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// arrow functions = a concise way to write function expressions
+//                                good for simple functions that you use only once
+//                                (parameters) => some code
+
 const hello = (name, age) => {console.log(`Hello ${name}`)
 console.log(`You are ${age} years old`)};
 
